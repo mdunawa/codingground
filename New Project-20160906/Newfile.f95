f9e7c9a21c3720805c7f72e08766ea026bc3c30f
mdunawa <mdunawa@gmail.com>
2016-09-06 16:33:13 -0400

    Created by Coding Ground Client

---
+++ New Project-20160906/Newfile.f95
@@ -0,0 +1,8 @@
+program first_program
+implicit none
+! The work done by this program appears below.
+write (*,*) 'This is an example'
+write (*,*) '     of'
+write (*,*) 'LIST DIRECTED write'
+stop
+end program first_program
